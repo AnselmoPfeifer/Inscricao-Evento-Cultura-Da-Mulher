bb86eba9edd41b65a7c2af79bf49aa0bef98e4e4
anselmopfeifer <anspfeifer@gmail.com>
2015-07-26 09:40:05 -0400

    Update Formulario.php

--- Connections/Formulario.php
+++ Connections/Formulario.php
@@ -6,5 +6,5 @@ $hostname_Formulario = "localhost";
 $database_Formulario = "cmo_culturadamulher";
 $username_Formulario = "root";
 $password_Formulario = "123";
-$Formulario = mysql_pconnect($hostname_Formulario, $username_Formulario, $password_Formulario) or trigger_error(mysql_error(),E_USER_ERROR);
+$Formulario = mysqli_connect($hostname_Formulario, $username_Formulario, $password_Formulario) or trigger_error(mysql_error(),E_USER_ERROR);
 ?>
